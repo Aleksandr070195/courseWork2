be59b0104f033660e0dc4fe00ebdd06ce296da26
Aleksandr <07_sanek_95@mail.ru>
2023-08-07 14:58:41 +0300

    /

---
+++ src/main/java/com/example/courseworke2/CourseWorke2Application.java
@@ -0,0 +1,13 @@
+package com.example.courseworke2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorke2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorke2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/courseworke2/CourseWorke2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseworke2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorke2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
